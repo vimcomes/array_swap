9109e982f8f09837d1fc8c5515f6acafe46ed2d0
Roman Chirkov <vimcomes@mail.ru>
2023-12-17 13:51:08 +0200

    swap ranges

--- main.cpp
+++ main.cpp
@@ -1,43 +1,34 @@
+/*
+ * little program to test 2 methods of swap vector arrays
+ * by std::swap_ranges
+ */
 #include <iostream>
 #include <vector>
 #include <chrono>
+#include <algorithm>
+
 
 class MyClass {
 public:
     std::vector<int> a;
-    std::vector<int> b;
-
-    void swap_by_reference() {
-        std::swap(a, b);
-    }
-
-    void swap_by_pointer() {
-        // Get pointers to the elements of the arrays
-        int* a_ptr = a.data();
-        int* b_ptr = b.data();
-
-        // Swap the pointers
-        std::swap(a_ptr, b_ptr);
-    }
 };
 
 int main() {
     MyClass my_class1;
     MyClass my_class2;
+    my_class1.a.resize(10000);
+    my_class2.a.resize(10000);
 
-    my_class1.a.resize(1000000);
-    my_class1.b.resize(1000000);
-
-    // Measure the time to swap the arrays by reference
+    //ref
     auto start = std::chrono::steady_clock::now();
-    my_class1.swap_by_reference();
+    std::ranges::swap_ranges(my_class1.a.begin(), my_class1.a.end(), my_class2.a.begin(), my_class2.a.end());
     auto end = std::chrono::steady_clock::now();
     std::chrono::duration<double> elapsed_time = end - start;
     std::cout << "Elapsed time to swap by reference: " << elapsed_time.count() << " seconds" << std::endl;
 
-    // Measure the time to swap the arrays by pointer
+    //pointer
     start = std::chrono::steady_clock::now();
-    my_class1.swap_by_pointer();
+    std::ranges::swap_ranges(my_class1.a.data(), my_class1.a.data() + my_class1.a.size(), my_class2.a.data(), my_class2.a.data() + my_class2.a.size());
     end = std::chrono::steady_clock::now();
     elapsed_time = end - start;
     std::cout << "Elapsed time to swap by pointer: " << elapsed_time.count() << " seconds" << std::endl;
